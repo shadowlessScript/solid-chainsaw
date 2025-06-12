544fadcc90967cec768fc7dfb4d4915637451ff6
shadowlessScript <munyasia225@gmail.com>
2025-06-12 11:52:19 +0300

    try api pagination 1

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ router.register('foundation',views.FoundationViewSet, basename='foundation')
 router.register('department',views.DepartmentViewSet, basename='department')
 router.register('reports',views.ReportsViewSet, basename='reports')
 router.register('analytics',views.AnalyticsViewSet, basename='analytics')
+router.register('pims-projects', views.ProjectsViewSet, basename='pims-projects')
    
                          
 urlpatterns = router.urls
